ec40983fc13d271ff9d1f5bdb05ff7742ecdc974
Andri Steiner <asteiner@snowflake.ch>
2015-03-31 14:35:43 +0200

    Release version 1.0.7

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "varnish".
  *
- * Auto generated 29-04-2014 16:16
+ * Auto generated 31-03-2015 14:34
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'This extension is managed on GitHub. Feel free to get in touch at https://github.com/snowflakech/typo3-varnish/',
 	'category' => 'misc',
 	'shy' => 0,
-	'version' => '1.0.6',
+	'version' => '1.0.7',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.3.0-0.0.0',
@@ -30,7 +30,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'author' => 'Andri Steiner',
 	'author_email' => 'varnish@snowflake.ch',
 	'author_company' => 'snowflake',
-	'_md5_values_when_last_written' => 'a:15:{s:16:"ext_autoload.php";s:4:"a0ec";s:21:"ext_conf_template.txt";s:4:"78a9";s:12:"ext_icon.gif";s:4:"a993";s:17:"ext_localconf.php";s:4:"cf97";s:13:"locallang.xml";s:4:"2529";s:10:"README.rst";s:4:"f6be";s:39:"classes/class.tx_varnish_controller.php";s:4:"6c71";s:33:"classes/class.tx_varnish_http.php";s:4:"1e50";s:45:"classes/Hooks/class.tx_varnish_hooks_ajax.php";s:4:"82d2";s:55:"classes/Hooks/class.tx_varnish_hooks_clearcachemenu.php";s:4:"95b8";s:48:"classes/Hooks/class.tx_varnish_hooks_tcemain.php";s:4:"9abd";s:49:"classes/Hooks/class.tx_varnish_hooks_tslib_fe.php";s:4:"6f01";s:53:"classes/Utilities/class.tx_varnish_generalutility.php";s:4:"6c64";s:15:"res/default.vcl";s:4:"9699";s:16:"static/setup.txt";s:4:"69d1";}',
+	'_md5_values_when_last_written' => 'a:16:{s:16:"ext_autoload.php";s:4:"a0ec";s:21:"ext_conf_template.txt";s:4:"d213";s:12:"ext_icon.gif";s:4:"a993";s:17:"ext_localconf.php";s:4:"975b";s:13:"locallang.xml";s:4:"1b38";s:10:"README.rst";s:4:"f6be";s:39:"classes/class.tx_varnish_controller.php";s:4:"92ff";s:33:"classes/class.tx_varnish_http.php";s:4:"9bf8";s:45:"classes/Hooks/class.tx_varnish_hooks_ajax.php";s:4:"82d2";s:55:"classes/Hooks/class.tx_varnish_hooks_clearcachemenu.php";s:4:"95b8";s:48:"classes/Hooks/class.tx_varnish_hooks_tcemain.php";s:4:"9abd";s:49:"classes/Hooks/class.tx_varnish_hooks_tslib_fe.php";s:4:"6e1a";s:53:"classes/Utilities/class.tx_varnish_generalutility.php";s:4:"0276";s:17:"res/default-4.vcl";s:4:"e648";s:15:"res/default.vcl";s:4:"4b55";s:16:"static/setup.txt";s:4:"69d1";}',
 );
 
 ?>
