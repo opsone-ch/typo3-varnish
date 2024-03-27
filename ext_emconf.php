28055ffdd59c8b6a695d9e3ee07b53131e51e975
Andri Steiner <andri@opsone.ch>
2024-03-27 14:57:10 +0100

    ext configuration #31: adjust PHP and TYPO3 dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,11 +9,11 @@ $EM_CONF[$_EXTKEY] = array (
     'description' => 'Seamless integration of TYPO3 cached pages into Varnish.' .
                      'Blazing fast response times for loads of simultaneous visitors.',
     'category' => 'misc',
-    'version' => '6.0.0',
+    'version' => '7.0.0',
     'constraints' => array (
         'depends' => array (
-            'php' => '7.4.0-8.2.99',
-            'typo3' => '11.5.0-12.5.99',
+            'php' => '8.1.0-8.3.99',
+            'typo3' => '12.4.0-13.5.99',
         ),
         'conflicts' => array (),
         'suggests' => array (),
