57455c078529a79acc2cef3d7f909a14611f4308
Sven Liebert <mail@sven-liebert.de>
2024-12-02 10:21:18 +0000

    Add initial support for PHP 8.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = array (
     'version' => '7.0.0',
     'constraints' => array (
         'depends' => array (
-            'php' => '8.1.0-8.3.99',
+            'php' => '8.1.0-8.4.99',
             'typo3' => '12.4.0-13.5.99',
         ),
         'conflicts' => array (),
