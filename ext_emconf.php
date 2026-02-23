f79c145eb7bd956a715f60d769c6af69c494d7fd
Mario Rimann <mario@opsone.ch>
2026-02-23 14:01:58 +0100

    Fix PHP constraint for TER
    see opsone_ch/typo3/varnish#46

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] =  [
     'version' => '8.0.0',
     'constraints' =>  [
         'depends' =>  [
-            'php' => '8.2.0-8.4.99',
+            'php' => '8.2.0-8.5.99',
             'typo3' => '13.4.0-14.3.99',
         ],
         'conflicts' =>  [],
